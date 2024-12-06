263c6f10800a95e1b792c56ea3df45aff270a6cc
Mattias Didriksson <mattias.didriksson@gmail.com>
2024-12-06 10:51:12 +0100

    Dag 5

--- day05/day05.fsx
+++ day05/day05.fsx
@@ -1,11 +1,12 @@
 open System
 open System.IO
-    
+
 
 let parseRule (l: String) =
-    l.Split ("|") |> Array.map int |> Array.toList
-let parseUpdates (l:String) = 
-    l.Split (",") |> Array.map int |> Array.toList
+    l.Split("|") |> Array.map int |> Array.toList
+
+let parseUpdates (l: String) =
+    l.Split(",") |> Array.map int |> Array.toList
 
 let checkRule (rule: int list) (update: int list) =
     let indexFst = update |> List.tryFindIndex (fun it -> it = rule[0])
@@ -14,40 +15,51 @@ let checkRule (rule: int list) (update: int list) =
     match indexFst, indexSnd with
     | Some val1, Some val2 -> val1 <= val2
     | _ -> true
-            
 
 let correctOrder (rules: int list list) (u: int list) =
     rules |> List.forall (fun r -> checkRule r u)
 
 let rawinput = File.ReadAllLines "day05/input.txt" |> Seq.toList
-let rules = 
-    rawinput 
-    |> List.takeWhile (fun x -> x <> "")
-    |> List.map parseRule
+let rules = rawinput |> List.takeWhile (fun x -> x <> "") |> List.map parseRule
 
-let updates= 
-    rawinput 
-    |> List.skip (1 + (List.length rules))
-    |> List.map parseUpdates
+let updates =
+    rawinput |> List.skip (1 + (List.length rules)) |> List.map parseUpdates
 
-let correctForRule (r: int list) (u: int list) =
-    let checkRule (rule: int list) (update: int list) =
-    let indexFst = update |> List.tryFindIndex (fun it -> it = rule[0])
-    let indexSnd = update |> List.tryFindIndex (fun it -> it = rule[1])
+let correctForRule (update: int list) (rule: int list) =
+    let isCorrect = checkRule rule update
 
-    match indexFst, indexSnd with
-    | Some val1, Some val2 -> val1 <= val2
-    | _ -> true
+    if isCorrect = true then
+        update
+    else
+        let indexFst = update |> List.findIndex (fun it -> it = rule[0])
+        let indexSnd = update |> List.findIndex (fun it -> it = rule[1])
 
-let reorderUpdate (u: int list) (rules: int list list) =
-    let breakingRules = rules |> List.filter (fun r -> (not (checkRule r u)))
-    breakingRules |> List.map (fun correctForRule r u)  
+        update
+        |> List.mapi (fun idx x ->
+            if idx = indexFst then update[indexSnd]
+            elif idx = indexSnd then update[indexFst]
+            else x)
 
-let partA updates = 
+let reorderUpdate (u: int list) (rules: int list list) = rules |> List.fold correctForRule u
+
+let partA updates =
     let matchingUpdates = updates |> List.filter (fun u -> correctOrder rules u)
-    matchingUpdates |> List.map (fun up -> up[List.length up / 2]) |> List.reduce (+)
 
-let incorrectUpdates = updates |> List.filter (fun u -> not (correctOrder rules u))
-incorrectUpdates |> List.map (fun iu -> reorderUpdate iu rules)
+    matchingUpdates
+    |> List.map (fun up -> up[List.length up / 2])
+    |> List.reduce (+)
 
+let checkForIncorrectUpdates updates rules =
+    updates |> List.filter (fun u -> not (correctOrder rules u))
 
+let rec doUntilCorrect (updates: int list list) (rules: int list list) =
+    let reordered = updates |> List.map (fun u -> reorderUpdate u rules)
+    let incorrect = checkForIncorrectUpdates reordered rules
+
+    match incorrect with
+    | [] -> reordered
+    | _ -> doUntilCorrect reordered rules
+
+let incorrect = checkForIncorrectUpdates updates rules
+let corrected = doUntilCorrect incorrect rules
+corrected |> List.map (fun up -> up[List.length up / 2]) |> List.reduce (+)
