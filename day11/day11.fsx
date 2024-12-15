d6c02fc57836fea8bf9c46716949f3ba3788ba9b
matdid <mattias.didriksson@lm.se>
2024-12-15 08:53:55 +0100

    Hade lite glömt memoizing/cache och fick göra det i f#. Kul. Inspirerades rätt rejält av internet för hur jag skulle gå tillväga. Dessutom fick jag ändra till att räkna stenarna istället för att avsluta med det.

--- day11/day11.fsx
+++ day11/day11.fsx
@@ -1,26 +1,39 @@
 open System
 open System.IO
 open Microsoft.FSharp.Core
+open System.Collections.Generic
 
 let rawInput = (File.ReadAllText "day11/input.txt").Split(" ") |> Array.map int64 |> Array.toSeq
 
-let blink (stones: int64 seq) =
-    stones |> Seq.collect (fun s -> 
-        let l = match s with
-                            | 0L -> [1 |> int64]
-                            | s when (string s |> String.length) % 2 = 0 -> 
-                                let strstone = string s 
-                                let l = strstone |> String.length
-                                [strstone[0..(l/2 - 1)];strstone[l/2..l]] |> List.map int64
-                            | _ -> [s * 2024L]
-        
-        l)
-let rec blinkTimes (stones: int64 seq) (times: int) = 
-    if times = 0 then
-        stones
-    else
-        let s = blink stones
-        let t = times - 1
-        blinkTimes s t
+let memoize f =
+    let cache = Dictionary<_, _>()
 
-blinkTimes  rawInput 75 |> Seq.toList |> List.length
+    fun x ->
+        match cache.TryGetValue x with
+        | true, value -> value
+        | false, _ ->
+            let value = f x
+            cache.Add(x, value)
+            value
+let blinkStone stone =
+    match stone with
+        | 0L -> [1L]
+        | s when (string s |> String.length) % 2 = 0 -> 
+            let strstone = string s 
+            let l = strstone |> String.length
+            [strstone[0..(l/2 - 1)];strstone[l/2..l]] |> List.map int64
+        | _ -> [stone * 2024L]
+let blinkfunction = memoize blinkStone
+let rec blinkTimes =
+    fun (times,stone) ->
+        if times = 0 then
+            1L
+        else
+            let ss = blinkfunction stone
+            let t = times - 1
+            ss |> Seq.sumBy (fun s -> blinkTimes  (t, s))
+    |> memoize
+
+
+let a = rawInput |> Seq.sumBy (fun s -> blinkTimes (25, s))
+let b = rawInput |> Seq.sumBy (fun s -> blinkTimes (75, s))
