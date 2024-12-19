62676da11698567e2ab6316c9f6947a7de456600
Mattias Didriksson <mattias.didriksson@gmail.com>
2024-12-19 22:58:48 +0100

    ugh

--- day14/day14.fsx
+++ day14/day14.fsx
@@ -36,28 +36,59 @@ let rec performTimes (robot: Robot) bounds times =
     else
         performTimes (performSecond robot bounds) bounds (times - 1)
 
-let bounds = (11, 7)
-
 let countForQuadrant (robots: Robot list) ((minx, miny), (maxx, maxy)) =
     robots
     |> List.filter (fun r ->
         let (x, y) = r.position
         x >= minx && x < maxx && y >= miny && y < maxy)
-    (*|> List.length*)
+    |> List.length
 
 let countQuadrants robots (maxx, maxy) =
     let upperleft = ((0, 0), (maxx / 2, maxy / 2))
-    let upperright = ((maxx / 2, 0), (maxx, maxy / 2))
-    let lowerleft = ((0, maxy / 2), (maxx / 2, maxy))
-    let lowerRight = ((maxx / 2, maxy / 2), (maxx, maxy))
-    [       lowerleft
-      (*
-      upperleft ;
-
-      lowerRight ; 
-      upperright *) ] |> List.map (countForQuadrant robots)
+    let upperright = ((maxx / 2 + 1, 0), (maxx, maxy / 2))
+    let lowerleft = ((0, maxy / 2 + 1), (maxx / 2, maxy))
+    let lowerRight = ((maxx / 2 + 1, maxy / 2 + 1), (maxx, maxy))
+    [ upperright ; lowerleft  ; upperleft ; lowerRight ]
+        |> List.map (countForQuadrant robots)
 
 let robots = File.ReadAllLines "day14/input.txt" |> Seq.toList |> List.map parse
 
-let robotsNewPosition = robots |> List.map (fun r -> performTimes r (11, 7) 100)
-countQuadrants robotsNewPosition bounds
+let bounds = (101, 103)
+
+
+let stringRow (robots: Robot list) row width =
+    seq {
+        for x in 0 .. width - 1 ->
+            let robot = robots |> List.tryFind (fun r -> r.position = (x, row))
+            match robot with
+            | None -> "."
+            | Some _ -> "#"
+    } |> Seq.concat |> Seq.map string |> String.concat ""
+
+let print robots (maxx, maxy) = 
+    seq {
+        for row in 0 .. maxy - 1 do
+            stringRow robots row maxx
+    }
+    |> Seq.toList
+          
+
+
+let robotsNewPosition = robots |> List.map (fun r -> performTimes r bounds 100)
+countQuadrants robotsNewPosition bounds |> List.reduce (*)
+
+
+let rec runTilPrint robots bounds times =
+    let res = robots |> List.map (fun r -> performTimes r bounds 1)
+    let prints = print res bounds            
+    if res |> List.map (_.position) |> List.distinct |> List.length = 500 then
+    //if prints |> List.exists (_.Contains("###############################")) = true then
+        prints |> List.map Console.WriteLine |> ignore
+        Console.WriteLine ("Times: " + (string times))
+        exit 1
+        runTilPrint res bounds (times + 1)        
+    else
+        runTilPrint res bounds (times + 1)
+
+(runTilPrint robots bounds 0) |> ignore 
+
